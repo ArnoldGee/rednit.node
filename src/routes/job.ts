2c0e8ac50058f5c529fbda5841ce619d53c844e4
Arnau Gómez <agomez@mrmilu.com>
2021-04-04 18:49:06 +0200

    feat(job router): delete job

--- src/routes/job.ts
+++ src/routes/job.ts
@@ -53,14 +53,14 @@ jobRouter.post("/", auth, async (req, res) => {
 jobRouter.put("/", auth, async (req, res) => {
   try {
     const { _userId, _id, ...job }:
-        & Omit<
-          IJob,
-          | "business"
-          | "creationDate"
-          | "selectedApplicants"
-          | "matchedApplicants"
-        >
-        & { _userId: string } = req.body; 
+      & Omit<
+        IJob,
+        | "business"
+        | "creationDate"
+        | "selectedApplicants"
+        | "matchedApplicants"
+      >
+      & { _userId: string } = req.body;
 
     const { name, place, sector, isRemote } = job;
     if (!(name && place && sector && isRemote)) {
@@ -69,7 +69,7 @@ jobRouter.put("/", auth, async (req, res) => {
       });
     }
 
-    const newJob = Job.findByIdAndUpdate(_id, job, {new: true})
+    const newJob = await Job.findByIdAndUpdate(_id, job, { new: true });
 
     res.status(200).json({
       msg: `Job ${job.name} was added to database`,
@@ -78,8 +78,26 @@ jobRouter.put("/", auth, async (req, res) => {
   } catch (err) {
     res.status(500).json({ msg: "Internal server error: " + err.message });
   }
-
 });
 
+jobRouter.delete("/", auth, async (req, res) => {
+  const { id }: { id: string } = req.body;
+
+  const job = await Job.findByIdAndDelete(id);
+
+  if (job) {
+    res.status(200).json({
+      msg: `Job ${job.name} was successfully deleted`,
+      job,
+    });
+  }
+
+  res.status(400).json(
+    {
+      msg:
+        `Job deletion failed: no job with id ${id} was registered in our database.`,
+    },
+  );
+});
 
 export default jobRouter;
